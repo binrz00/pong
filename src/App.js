d4f558d2b188f991c1137a7c2c098bda1baeea28
Brian Zauner <binrz00@hotmail.com>
2020-01-23 14:30:28 -0600

    made a ball
    made a ball

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./styles.css";
 import Paddle from "./components/paddle";
 
 export default function App() {
+  const [p1PaddleY, setp1PaddleY] = useState(0);
+  const [p2PaddleY, setp2PaddleY] = useState(0);
+  function handleKey(event) {
+    switch (event.keyCode) {
+      case 87:
+        setp1PaddleY(p1PaddleY + 1);
+        break;
+      case 38:
+        setp2PaddleY(p2PaddleY + 1);
+        break;
+      case 83:
+        setp1PaddleY(p1PaddleY - 1);
+        break;
+      case 40:
+        setp2PaddleY(p2PaddleY - 1);
+        break;
+    }
+  }
+  useEffect(() => {
+    window.addEventListener("keydown", handleKey);
+  }, [p1PaddleY, p2PaddleY, handleKey]);
   return (
     <div className="container">
-      <Paddle />
-      <Paddle isPlayerTwo />
+      <Paddle paddleY={p1PaddleY} />
+      <Paddle paddleY={p2PaddleY} isPlayerTwo />
     </div>
   );
 }
